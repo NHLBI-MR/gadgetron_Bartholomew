b14aadf8768ba643c58719a08b149cb1847365a1
Hui Xue <hui.xue@nih.gov>
2017-12-01 10:19:51 -0500

    adjust demo

--- demo.cpp
+++ demo.cpp
@@ -8,5 +8,6 @@ int main(int argc, char** argv)
     GDEBUG_STREAM(" --------------------------------------------------------------------------------------------------");
     GDEBUG_STREAM("This is the demo for the gadgetron-Bartholomew collaboration ... ");
     GDEBUG_STREAM(" --------------------------------------------------------------------------------------------------");
+
     return 0;
 }
