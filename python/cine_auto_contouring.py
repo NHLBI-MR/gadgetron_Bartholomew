8a1c2f92625c4c9ded77f762ac62635c45bb91fd
Hui Xue <hui.xue@nih.gov>
2017-12-08 22:04:25 -0500

    refine contouring py file

--- python/cine_auto_contouring.py
+++ python/cine_auto_contouring.py
@@ -17,11 +17,12 @@ class CineContouring(Gadget):
 
     def process_config(self, cfg):
         print("Process config of cine contouring ... ")
-        self.images = []
-        self.headers = []
-        self.metas = []
 
-        self.header = ismrmrd.xsd.CreateFromDocument(cfg)
+        try:
+            self.header = ismrmrd.xsd.CreateFromDocument(cfg)
+        except:
+            print('CineContouring, parse xml header failed ... ')
+            return 1
 
         # first encoding space
         self.enc = self.header.encoding[0]
@@ -32,6 +33,7 @@ class CineContouring(Gadget):
         self.slc = self.enc.encodingLimits.slice.maximum+1
         self.phs = self.enc.encodingLimits.phase.maximum+1
         self.phs_retro = int(self.params["phase"])
+        self.send_origin = bool(self.params["send_original"])
 
         print("CineContouring, maximal number of slice ", self.slc)
 
@@ -43,57 +45,63 @@ class CineContouring(Gadget):
                 self.phs_retro = ss[1]
 
         print("CineContouring, number of retro-gated phases ", self.phs_retro)
-                                                                                        
+
+        # instansiate tensorflow model to avoid delays in loading parameters later
+        try:
+            start = time.time()
+            self.segmentation_model = SegmentationModel()
+            end = time.time()
+            print('CineContouring, loading model : ', end-start)
+        except:
+            print('CineContouring, load model failed ... ')
+            return 1
+
     def process(self, header, image, metadata=None):
         print("Receiving image__+_, phase ", header.phase, ", slice ", header.slice, ', data size', image.shape)
 
-        # buffer incoming images
-        self.images.append(image)
-        self.headers.append(header)
+        pixel_spacing = header.field_of_view[0]/header.matrix_size[0] # cheat with a single dimension for now...
+        sa_slice_index=header.slice
+        sa_phase_index=header.phase
 
-	pixel_spacing = header.field_of_view[0]/header.matrix_size[0] # cheat with a single dimension for now...
-	sa_slice_index=header.slice
-	sa_phase_index=header.phase
-
-	# instansiate tensorflow model to avoid delays in loading parameters later
-	if len(self.images)==1: 	# hard-code so that it's only called once
+        try:
             start = time.time()
-	    self.segmentation_model = SegmentationModel()
+            # row-wise to column-wise
+            image_column_wise = np.transpose(image, (1,0,2,3))
+            ctr_endo_y, ctr_endo_x, ctr_epi_y, ctr_epi_x, _, _ = segment_single_image (image_column_wise, pixel_spacing, sa_slice_index, sa_phase_index, self.segmentation_model)
             end = time.time()
-            print('CineContouring, loading model : ', end-start)
+            print('Cine contouring, segment image : ', end-start)
+        except:
+            print('CineContouring, segment failed ... ')
+            return 1
 
-        start = time.time()
-        # row-wise to column-wise
-        image_column_wise = np.transpose(image, (1,0,2,3))
-        ctr_endo_y, ctr_endo_x, ctr_epi_y, ctr_epi_x, _, _ = segment_single_image (image_column_wise, pixel_spacing, sa_slice_index, sa_phase_index, self.segmentation_model)
-        end = time.time()
-        print('Cine contouring, segment image : ', end-start)
-        
-	if metadata is not None:
-            # deserialize metadata
-	    curr_meta = ismrmrd.Meta.deserialize(metadata)
-	    updated_meta = add_contours_to_single_header (curr_meta, ctr_endo_x, ctr_endo_y, ctr_epi_x, ctr_epi_y)
-            self.metas.append(updated_meta)
+        # deserialize metadata
+        curr_meta = ismrmrd.Meta.deserialize(metadata)
 
-        # if all images are received
-        if header.slice<self.slc-1 or header.phase<self.phs_retro-1:
-            self.put_next(header,image,curr_meta)
-            return 0
+        # attach contours
+        updated_meta = add_contours_to_single_header (curr_meta, ctr_endo_x, ctr_endo_y, ctr_epi_x, ctr_epi_y)
 
-        # send out the last image
-        self.put_next(header,image,curr_meta)
+        header_sent = header
+        image_sent = image
 
-	# send out copy of image with contour
-	for i in range(0,len(self.metas)):
-            self.headers[i].image_series_index += 2000 
-	    this_meta=self.metas[i]
-            if(this_meta.has_key('GADGETRON_ImageComment')):
-                image_comment = this_meta['GADGETRON_ImageComment']
+        # if send original image
+        try:
+            if self.send_original:
+                self.put_next(header,image,curr_meta)
+        except:
+            print('CineContouring, send original image to downstream failed ... ')
+            return 1
+
+        try:
+            # send out copy of image with contour
+            header_sent.image_series_index += 2050 
+            if(updated_meta.has_key('GADGETRON_ImageComment')):
+                image_comment = updated_meta['GADGETRON_ImageComment']
                 image_comment.append('ENDO_EPI')
-            self.put_next(self.headers[i],self.images[i],this_meta)
+                updated_meta['GADGETRON_ImageComment']=image_comment
 
+            self.put_next(header_sent,image_sent,updated_meta)
+        except:
+            print('CineContouring, send image with contours to downstream failed ... ')
+            return 1
 
         return 0
-
-
-
